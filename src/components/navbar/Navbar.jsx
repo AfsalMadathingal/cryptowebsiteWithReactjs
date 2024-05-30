b5b61e213a4fa53575e15b64eca9280ab0e831a5
afsal <madathingalafsal@gmail.com>
2024-05-30 16:35:07 +0530

    completed project

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,30 +1,64 @@
 import "./navbar.css";
 
-import React from "react";
+import React, { useContext } from "react";
 import logo from "../../assets/logo.png";
 import arrow from "../../assets/arrow_icon.png";
+import  {CoinContext} from "../../context/CoinContext";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+
+  const { currency ,setCurrency } = useContext(CoinContext);
+
+  const currecyHandler = (e) => {
+
+    console.log(e.target.value);
+
+    switch (e.target.value) {
+
+      case "usd": {
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      }
+      case "eur": {
+        setCurrency({ name: "eur", symbol: "€" });
+        break;
+      }
+      case "inr": {
+        setCurrency({ name: "inr", symbol: "₹" });
+        break;
+      }
+      default: {
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      }
+    }
+  };
+
   return (
     <div className="navbar">
+      <Link to={'/'}>
       <img src={logo} className="logo" alt="" />
+      </Link>
       <ul>
+        <Link>
         <li>Home</li>
+        </Link>
         <li>Features</li>
         <li>Pricing</li>
         <li>Blog</li>
       </ul>
       <div className="nav-right">
-        <select name="" id="">
-            <option value="usd">USD</option>
-            <option value="eur">EUR</option>
-            <option value="gbp">INR</option>
-
+        <select onChange={currecyHandler} name="" id="">
+          <option value="usd">USD</option>
+          <option value="eur">EUR</option>
+          <option value="inr">INR</option>
         </select>
-        <button>Sign Up  <img src={arrow} alt="" /></button>
+        <button>
+          Sign Up <img src={arrow} alt="" />
+        </button>
       </div>
     </div>
-    
   );
 };
 
--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,13 +1,103 @@
-import React from 'react'
-import './Coin.css'
-
+import React, { useContext, useEffect, useState } from "react";
+import "./Coin.css";
+import { CoinContext } from "../../context/CoinContext";
+import { useParams } from "react-router-dom";
+import ReactLoading from "react-loading";
+import LineChart from "../../components/linechart/LineChart";
 
 const Coin = () => {
+  const { coinId } = useParams();
+  const [coindData, setCoinData] = useState();
+  const { currency } = useContext(CoinContext);
+  const [history, setHistory] = useState();
+
+  const fetchCoinData = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": "CG-zQULtXWTHk3dvmHCuTDEwkNH",
+      },
+    };
+
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
+      .then((response) => response.json())
+      .then((response) => setCoinData(response))
+      .catch((err) => console.error(err));
+  };
+
+  const fetchHistory = () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": "CG-zQULtXWTHk3dvmHCuTDEwkNH",
+      },
+    };
+
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10&interval=daily`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response);
+        setHistory(response);
+      })
+
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    fetchCoinData();
+    fetchHistory();
+  }, [currency]);
+
+  if (!coindData)
+    return (
+      <div className="loading">
+        {" "}
+        <ReactLoading type="spin" color="blue" height={"5%"} width={"5%"} />
+      </div>
+    );
+
   return (
-    <div>
-      <h1>COING</h1>
+    <div className="coin">
+      <div className="coin-name">
+        <img src={coindData?.image?.large} alt="" />
+        <p>
+          <b>
+            {coindData?.name}({coindData?.symbol.toUpperCase()})
+          </b>
+        </p>
+      </div>
+      <div className="coin-chart">
+        <LineChart historicalData={history} />
+      </div>
+      <div className="coin-info">
+        <ul>
+          <li>Crypto Market Rank</li>
+          <li>Rank : {coindData?.market_cap_rank}</li>
+        </ul>
+        <ul>
+          <li>Current Price</li>
+          <li>{currency.symbol} {coindData?.market_data?.current_price[currency.name].toLocaleString()}</li>
+        </ul>
+        <ul>
+          <li>Market Cap</li>
+          <li>{currency.symbol} {coindData?.market_data?.market_cap[currency.name].toLocaleString()}</li>
+        </ul>
+        <ul>
+          <li>24h High</li>
+          <li>{currency.symbol} {coindData?.market_data?.high_24h[currency.name].toLocaleString()}</li>
+        </ul>
+        <ul>
+          <li>24h Low</li>
+          <li>{currency.symbol} {coindData?.market_data?.low_24h[currency.name].toLocaleString()}</li>
+        </ul>
+      </div>
     </div>
-  )
-}
-import './Coin.css'
-export default Coin
+  );
+};
+
+export default Coin;
